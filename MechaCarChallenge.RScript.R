13b63246ab46a0d7d66de15627f0309f9051db3e
abbys114 <75815115+abbys114@users.noreply.github.com>
2021-04-10 10:51:17 -0700

    Add files via upload

---
+++ MechaCarChallenge.RScript.R
@@ -0,0 +1,66 @@
+library(dplyr)
+library(dplyr)
+
+Attaching package: ‘dplyr’
+
+The following objects are masked from ‘package:stats’:
+  
+  filter, lag
+
+The following objects are masked from ‘package:base’:
+  
+  intersect, setdiff, setequal, union
+
+> mpg <- read.csv('MechaCar_mpg.csv',stringsAsFactors = F) # loading mpg dataset
+> lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mpg) # create linear regression
+
+Call:
+  lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+       ground_clearance + AWD, data = mpg)
+
+Coefficients:
+  (Intercept)    vehicle_length    vehicle_weight     spoiler_angle  
+-1.040e+02         6.267e+00         1.245e-03         6.877e-02  
+ground_clearance               AWD  
+3.546e+00        -3.411e+00  
+
+> 
+  > summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD, mpg)) # summarize linear regression
+
+Call:
+  lm(formula = mpg ~ vehicle_length + vehicle_weight + spoiler_angle + 
+       ground_clearance + AWD, data = mpg)
+
+Residuals:
+  Min       1Q   Median       3Q      Max 
+-19.4701  -4.4994  -0.0692   5.4433  18.5849 
+
+Coefficients:
+  Estimate Std. Error t value Pr(>|t|)    
+(Intercept)      -1.040e+02  1.585e+01  -6.559 5.08e-08 ***
+  vehicle_length    6.267e+00  6.553e-01   9.563 2.60e-12 ***
+  vehicle_weight    1.245e-03  6.890e-04   1.807   0.0776 .  
+spoiler_angle     6.877e-02  6.653e-02   1.034   0.3069    
+ground_clearance  3.546e+00  5.412e-01   6.551 5.21e-08 ***
+  AWD              -3.411e+00  2.535e+00  -1.346   0.1852    
+---
+  Signif. codes:  0 ‘***’ 0.001 ‘**’ 0.01 ‘*’ 0.05 ‘.’ 0.1 ‘ ’ 1
+
+Residual standard error: 8.774 on 44 degrees of freedom
+Multiple R-squared:  0.7149,	Adjusted R-squared:  0.6825 
+F-statistic: 22.07 on 5 and 44 DF,  p-value: 5.35e-11
+
+> sus_coils <- read.csv('Suspension_Coil.csv', stringsAsFactors = F) # loading suspension coil dataset
+> total_summary <- sus_coils %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) # Create a summary for all PSI data
+> lot_summary <- sus_coils %>% group_by(Manufacturing_Lot) %>% summarize(Mean=mean(PSI), Median=median(PSI), Variance=var(PSI), SD=sd(PSI)) # Create a summary for all PSI data by lot
+
+t.test(x=sus_coils$PSI, mu=1500) # t test n all cars' PSI with a mean of 1500
+pop1 <- subset(sus_coils, Manufacturing_Lot=='Lot1') # Create subset for lot 1 cars
+t.test(x=pop1$PSI, mu=1500) # t test on lot 1 cars' PSI with a mean of 1500
+pop2 <- subset(sus_coils, Manufacturing_Lot=='Lot2') # Create subset for lot 2 cars
+t.test(x=pop2$PSI, mu=1500) # t test on lot 2 cars' PSI with a mean of 1500
+pop3 <- subset(sus_coils, Manufacturing_Lot=='Lot3') # Create subset for lot 3 cars
+t.test(x=pop3$PSI, mu=1500) # t test on lot 3 cars' PSI with a mean of 1500
+# Comparing MechaCar to the Competition
+
+
